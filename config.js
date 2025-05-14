c6d3afb3f263ebdca74c9f9cafc3157f058a1463
nishikinishiki <r-nishiki@jpreturns.com>
2025-05-14 20:59:31 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+    // 設定ファイル (config.js)
+    // このファイルに、ご自身の環境に合わせて値を設定してください。
+
+    // 1. Google Apps ScriptのウェブアプリURL
+    // 例: 'https://script.google.com/macros/s/XXXXXXXXXXXXXXXXXXXXYYYYYYYYYYYYZZZZZZZZZZ/exec'
+    const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyX7vWqOR0QmlH9LHJYUBFnXI6C271wpk17eSKu_4akiFl61fD2vyxjWB2tDCrznyChxw/exec';
+
+    // 2. 質問者アイコンの画像URL
+    // 例: 'images/やったー (1).png' (HTMLファイルからの相対パス)
+    // 例: 'https://あなたのユーザー名.github.io/リポジトリ名/images/やったー (1).png' (絶対パス)
+    const BOT_ICON_URL = 'images/icon.png';
+    
