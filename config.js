45ccb0fa123191278f80b0d2a61c93bde352bc9a
nishikinishiki <r-nishiki@jpreturns.com>
2025-05-15 14:08:29 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,17 @@
-    // 設定ファイル (config.js)
-    // このファイルに、ご自身の環境に合わせて値を設定してください。
+// 設定ファイル (config.js)
+// このファイルに、ご自身の環境に合わせて値を設定してください。
 
-    // 1. Google Apps ScriptのウェブアプリURL
-    // 例: 'https://script.google.com/macros/s/XXXXXXXXXXXXXXXXXXXXYYYYYYYYYYYYZZZZZZZZZZ/exec'
-    const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyX7vWqOR0QmlH9LHJYUBFnXI6C271wpk17eSKu_4akiFl61fD2vyxjWB2tDCrznyChxw/exec';
+// 1. Google Apps ScriptのウェブアプリURL
+// 例: 'https://script.google.com/macros/s/XXXXXXXXXXXXXXXXXXXXYYYYYYYYYYYYZZZZZZZZZZ/exec'
+const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyX7vWqOR0QmlH9LHJYUBFnXI6C271wpk17eSKu_4akiFl61fD2vyxjWB2tDCrznyChxw/exec';
 
-    // 2. 質問者アイコンの画像URL
-    // 例: 'images/やったー (1).png' (HTMLファイルからの相対パス)
-    // 例: 'https://あなたのユーザー名.github.io/リポジトリ名/images/やったー (1).png' (絶対パス)
-    const BOT_ICON_URL = 'images/icon.png';
-    
+// 2. 質問者アイコンの画像URL
+// 例: 'images/やったー (1).png' (HTMLファイルからの相対パスでimagesフォルダ内にある場合)
+// 例: 'やったー (1).png' (HTMLファイルと同じ階層に画像を置く場合)
+// 例: 'https://あなたのユーザー名.github.io/リポジトリ名/images/やったー (1).png' (絶対パス)
+const BOT_ICON_URL = 'images/icon.png';
+
+// 3. ファビコンの画像URL
+// 例: 'images/favicon.png' (HTMLファイルからの相対パスでimagesフォルダ内にある場合)
+// 例: 'favicon.png' (HTMLファイルと同じ階層に画像を置く場合)
+const FAVICON_URL = 'images/favicon.png'; 
