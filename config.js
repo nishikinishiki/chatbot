8ad2baad4a1412fa33b1689b2a276f5afcf0e9c9
nishikinishiki <r-nishiki@jpreturns.com>
2025-05-16 13:28:41 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // 設定ファイル (config.js)
 
 // 1. Google Apps ScriptのウェブアプリURL
-const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyX7vWqOR0QmlH9LHJYUBFnXI6C271wpk17eSKu_4akiFl61fD2vyxjWB2tDCrznyChxw/exec';
+const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwMhm7EyoiVCWQ6ZGPkxefvV9L1uNqN6ynZdYqirDHqTdHcbu-WNzDWu33VT3uQXP_TDw/exec';
 // 2. 質問者アイコンの画像URL
 const BOT_ICON_URL = 'images/icon.png';
 // 3. ファビコンの画像URL
