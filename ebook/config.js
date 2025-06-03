14b18970d5443154c29bcb3ac6789b346319aed8
nishikinishiki <r-nishiki@jpreturns.com>
2025-06-03 13:09:49 +0900

    images/に変更

--- ebook/config.js
+++ ebook/config.js
@@ -3,6 +3,6 @@
 // 1. Google Apps ScriptのウェブアプリURL
 const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyK_txJ1HSlyOrToH9ziWCIqOG9g-5oDCYtcZfXq8P5FRkJbXMLY_8yIilihuIXKOSU/exec';
 // 2. 質問者アイコンの画像URL
-const BOT_ICON_URL = 'images/icon.png';
+const BOT_ICON_URL = 'ebook/images/icon.png';
 // 3. ファビコンの画像URL
-const FAVICON_URL = 'images/favicon.png'; 
+const FAVICON_URL = 'ebook/images/favicon.png'; 
