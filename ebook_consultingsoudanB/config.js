3d7558a450b03f11a6f4748458ade129d24a863c
nishikinishiki <r-nishiki@jpreturns.com>
2025-07-16 17:51:58 +0900

    Update config.js

--- ebook_consultingsoudanB/config.js
+++ ebook_consultingsoudanB/config.js
@@ -7,4 +7,4 @@ const BOT_ICON_URL = 'images/icon.png';
 // 3. ファビコンの画像URL
 const FAVICON_URL = 'images/favicon.png'; 
 // 4. 【新規追加】チャット開始時に表示するバナー画像のURL
-const BANNER_IMAGE_URL = 'images/banner.webp'; 
+const BANNER_IMAGE_URL = 'images/chatbot_gift.png'; 
