af805c48d270e03fc8f3618a2835949afd1c394c
nishikinishiki <r-nishiki@jpreturns.com>
2025-09-01 20:22:07 +0900

    送信完了メッセージを更新し、ユーザーへの感謝のメッセージを削除

--- consultation/js/main.js
+++ consultation/js/main.js
@@ -334,7 +334,6 @@ async function submitDataToGAS(dataToSend) {
 
         clearChatMessages();
         await addBotMessage("送信が完了しました。<br>お問い合わせいただきありがとうございました！", true);
-        await addBotMessage("全ての情報を承りました。ご回答ありがとうございました！<br>後ほど担当よりご連絡いたします。", true);
         await addBotMessage("お問い合わせはお電話でも受け付けております。<br>電話番号：<a href='tel:0120147104'>0120-147-104</a><br>営業時間：10:00～22:00（お盆・年末年始除く）", true);
         await addBotMessage("デジタル書籍は下記から閲覧できます！");
         await addBotMessage("デジタル書籍を閲覧する", false, false, true);
