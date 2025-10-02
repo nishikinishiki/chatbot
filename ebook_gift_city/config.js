1c762f3f48b20cdfe894feb1859d432aa622aae6
nishikinishiki <r-nishiki@jpreturns.com>
2025-10-02 19:02:34 +0900

    デプロイURL更新

--- ebook_gift_city/config.js
+++ ebook_gift_city/config.js
@@ -1,7 +1,7 @@
 // 設定ファイル (config.js)
 
 // 1. direct Pardot Form Handler URL
-const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzyQCOwIJeE34fAMIbD7xvRN_4qGSdV4NAc0_DL9vycepCq7orKcUOMJtb1COIcd7Aw/exec';
+const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycby18ywbiYHXSvlqiKgh9BCyN-BmQMxhKVJvuQIg9wafDZoyD56NhlYSZvt568Wcvjz4/exec';
 
 // 2. Google Apps ScriptのウェブアプリURL（ログデータ用）
 const GAS_LOG_APP_URL = 'https://script.google.com/macros/s/AKfycbxOEisLnlCPIKn18Cgoli1dCRxyXfrjG3BOp68Ne4G5RzY1s8rYgVMzAiB90RsMZoif/exec';
