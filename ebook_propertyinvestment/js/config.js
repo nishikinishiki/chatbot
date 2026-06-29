99b4789311a6053f7944e18521470eb16dafd442
nishikinishiki <r-nishiki@jpreturns.com>
2026-06-29 19:50:15 +0900

    feat: 新しいバナー画像と動画を追加し、キャンペーン用の設定を更新

--- ebook_propertyinvestment/js/config.js
+++ ebook_propertyinvestment/js/config.js
@@ -20,6 +20,10 @@ const BANNER_CAMPAIGN = {
     'like_dining': '/ebook_propertyinvestment/images/banner_ebook_like-dining.png',
     'like_suv': '/ebook_propertyinvestment/images/banner_ebook_like-suv.png',
     'like_watch': '/ebook_propertyinvestment/images/banner_ebook_like-watch.png',
+    'no1_': [
+        '/ebook_propertyinvestment/images/banner_no1.mp4',
+        '/ebook_propertyinvestment/images/banner_gift.webp'
+    ],
     'fbtrg': '/ebook_propertyinvestment/images/banner_ebook_meta.webp',
     'ALA_gift_ebook_2529PM': '/ebook_propertyinvestment/images/banner_chotoshin_260625.webp',
     'ALA_gift_ebook_2529y': '/ebook_propertyinvestment/images/banner_chotoshin_260625.webp',
