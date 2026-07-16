2b8727255ff9534ae6ad62ce30774d38e436aef4
nishikinishiki <r-nishiki@jpreturns.com>
2026-07-16 10:14:35 +0900

    新しいHTMLファイルと設定ファイルを追加し、Google Tag Managerを統合

---
+++ ebook_renovation/js/config.js
@@ -0,0 +1,36 @@
+// 1. GAS
+const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyigqfQx0uNHXUIECwD3CxWEPT1Q4mpqJhABdvt8B2DFIdDDXWT3NmdxbdWPX5iHtV3AA/exec';
+
+// 2. ファビコン
+const FAVICON_URL = '/common/images/favicon.png';
+
+// 3. 質問者アイコン
+const ICON_DEFAULT = '/common/images/icon_woman.png';
+const ICON_CAMPAIGN = {
+    'fbtrg': '/common/images/icon_woman.png'
+};
+
+// 4. バナー
+const BANNER_DEFAULT = [
+    '/common/images/banner_no1.mp4',
+    '/common/images/banner_gift.webp'
+];
+const BANNER_CAMPAIGN = {
+    'ALA_gift_ebook_4362PM': '/common/images/banner_buzz_260512.webp',
+    'ALA_gift_ebook_4362': '/common/images/banner_buzz_260512.webp',
+    'like_dining': '/common/images/banner_ebook_like-dining.png',
+    'like_suv': '/common/images/banner_ebook_like-suv.png',
+    'like_watch': '/common/images/banner_ebook_like-watch.png',
+    'fbtrg': '/common/images/banner_ebook_meta.webp',
+    'ALA_gift_ebook_2529PM': '/common/images/banner_chotoshin_260625.webp',
+    'ALA_gift_ebook_2529y': '/common/images/banner_chotoshin_260625.webp',
+    'ALA_gift_ebook_2529': '/common/images/banner_chotoshin_260625.webp',
+    'wk_gift_ebook': '/common/images/banner_WEK.png'
+};
+
+// 5. CSS
+const STYLE_DEFAULT = '/common/css/style-2.css';
+const STYLE_CAMPAIGN = {
+    'fbtrg': '/common/css/style-2.css',
+    'wk_gift_ebook': '/common/css/style-2.css',
+};
